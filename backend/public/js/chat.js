04b711f332e175596c63b8ecc610de5c19893aa2
Brais <braiscao.dev@gmail.com>
2020-12-09 21:47:24 +0100

    Added basic functionality

--- backend/public/js/chat.js
+++ backend/public/js/chat.js
@@ -1,2 +1,13 @@
 // eslint-disable-next-line no-undef
-io();
+const socket = io();
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const message = e.target.elements.message.value;
+
+  socket.emit('sendMessage', message);
+});
+
+socket.on('message', (data) => {
+  console.log(data);
+});
