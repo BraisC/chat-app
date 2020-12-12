080d60a45e4f2570e897590085dbb14390271568
Brais <braiscao.dev@gmail.com>
2020-12-12 13:13:20 +0100

    Added some more acknowledgements to enable disabling button

--- backend/public/js/chat.js
+++ backend/public/js/chat.js
@@ -1,21 +1,31 @@
 // eslint-disable-next-line no-undef
 const socket = io();
 
+const form = document.querySelector('#message-form');
+const formInput = form.querySelector('input');
+const formButton = form.querySelector('button');
+const locationButton = document.querySelector('#location-button');
+
 socket.on('message', (data) => {
   console.log(data);
 });
 
-document.querySelector('#message-form').addEventListener('submit', (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
+  formButton.setAttribute('disabled', true);
   const message = e.target.elements.message.value;
 
   //La funcion es una callback que se ejecuta cuando el evento se reciba
   socket.emit('sendMessage', message, (mes) => {
     console.log(`Message delivered: ${mes}`);
+    formButton.removeAttribute('disabled');
+    formInput.value = '';
+    formInput.focus();
   });
 });
 
-document.querySelector('#location-button').addEventListener('click', () => {
+locationButton.addEventListener('click', () => {
+  locationButton.setAttribute('disabled', true);
   navigator.geolocation.getCurrentPosition((position) => {
     socket.emit(
       'sendLocation',
@@ -25,6 +35,7 @@ document.querySelector('#location-button').addEventListener('click', () => {
       },
       () => {
         console.log(`Location shared!!`);
+        locationButton.removeAttribute('disabled');
       }
     );
   });
