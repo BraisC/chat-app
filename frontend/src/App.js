e1ddcad98077e25e983c29da23b177181f788304
Brais <braiscao.dev@gmail.com>
2020-12-12 23:21:26 +0100

    Initialized connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,30 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 import './App.css';
 
 function App() {
-  useEffect(() => {
-    const socket = io('http://127.0.0.1:3000');
-    socket.emit('joinRoom', { username: 'manolo', roomname: '2' }, (error) => {
-      if (error) {
-        alert(error);
-      }
-    });
-  }, []);
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+
+  const socket = io('http://127.0.0.1:3000');
+  useEffect(() => {}, []);
 
   console.log('si');
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Chat
-        </a>
-      </header>
+      <input value={name} onChange={(e) => setName(e.target.value)} type="text" />
+      <input value={room} onChange={(e) => setRoom(e.target.value)} type="text" />
+      <div
+        onClick={() => {
+          socket.emit('joinRoom', { username: name, roomname: room }, (error) => {
+            if (error) {
+              alert(error);
+            }
+          });
+        }}
+      >
+        Clickme
+      </div>
     </div>
   );
 }
