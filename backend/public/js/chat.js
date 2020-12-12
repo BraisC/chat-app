3cfdde81ff92659a4e76aae0645181628ad76f79
Brais <braiscao.dev@gmail.com>
2020-12-12 12:43:43 +0100

    Added some event acknowledgements

--- backend/public/js/chat.js
+++ backend/public/js/chat.js
@@ -9,14 +9,23 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
   e.preventDefault();
   const message = e.target.elements.message.value;
 
-  socket.emit('sendMessage', message);
+  //La funcion es una callback que se ejecuta cuando el evento se reciba
+  socket.emit('sendMessage', message, (mes) => {
+    console.log(`Message delivered: ${mes}`);
+  });
 });
 
 document.querySelector('#location-button').addEventListener('click', () => {
   navigator.geolocation.getCurrentPosition((position) => {
-    socket.emit('sendLocation', {
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude,
-    });
+    socket.emit(
+      'sendLocation',
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      () => {
+        console.log(`Location shared!!`);
+      }
+    );
   });
 });
--- backend/src/index.js
+++ backend/src/index.js
@@ -15,15 +15,20 @@ app.use(express.static(publicDirectoryPath));
 io.on('connection', (socket) => {
   console.log('New websocket connection');
 
+  //Emite a todos los sockets
   socket.emit('message', 'Welcome!');
+  //Emite a todos los sockets menos al actual
   socket.broadcast.emit('message', 'A new user has joined');
 
-  socket.on('sendMessage', (message) => {
+  //La callback es la función de acknowledgement enviada por el emisor
+  socket.on('sendMessage', (message, callback) => {
     io.emit('message', message);
+    callback('Delivered');
   });
 
-  socket.on('sendLocation', (location) => {
+  socket.on('sendLocation', (location, callback) => {
     io.emit('message', `https://google.com/maps?q=${location.latitude},${location.longitude}`);
+    callback();
   });
 
   socket.on('disconnect', () => {
