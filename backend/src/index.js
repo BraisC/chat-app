83892a688d6f36ed976e3db508441c0e31aa4d21
Brais <braiscao.dev@gmail.com>
2020-12-13 01:53:43 +0100

    Cleaned up backend, removed express dep

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,12 @@
-const path = require('path');
 const http = require('http');
-const express = require('express');
 const socketio = require('socket.io');
 const { generateMessage } = require('./utils/messages');
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/users');
 
-const app = express();
-const server = http.createServer(app);
+const server = http.createServer();
 const io = socketio(server);
 
-const port = process.env.PORT || 3000;
-const publicDirectoryPath = path.join(__dirname, '../public');
-
-app.use(express.static(publicDirectoryPath));
+const port = process.env.PORT || 4001;
 
 io.on('connection', (socket) => {
   console.log('New websocket connection');
